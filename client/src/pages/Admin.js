43a724469efc209e34b0b0cb09cf740991c1e839
lesuuda <lesuudaljamani@gmail.com>
2024-07-25 11:18:38 +0300

    core: set up admin and cashier pages with minor fixes

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,126 @@
+import "./Admin.css";
+
+const Admin = () => {
+  return (
+    <div className="home">
+      <div className="admin-sidebar">
+        <div className="refunds">Refunds</div>
+        <img className="refund-icon" alt="" src="/refund@2x.png" />
+        <div className="categories">Categories</div>
+        <div className="logout">Logout</div>
+        <div className="credits">Credits</div>
+        <div className="suppliers">Suppliers</div>
+        <div className="products">Products</div>
+        <div className="reports">Reports</div>
+        <div className="stores">Stores</div>
+        <div className="users">Users</div>
+        <div className="pos-system">POS SYSTEM</div>
+        <div className="dashboard">Dashboard</div>
+        <img className="home-fill-icon1" alt="" src="/home-fill@2x.png" />
+        <img
+          className="grocery-store-icon"
+          alt=""
+          src="/grocery-store@2x.png"
+        />
+        <img className="users-icon" alt="" src="/users@2x.png" />
+        <img className="liquor-shelf-icon" alt="" src="/liquor-shelf@2x.png" />
+        <img className="line-chart-icon" alt="" src="/line-chart@2x.png" />
+        <img className="debt-icon" alt="" src="/debt@2x.png" />
+        <img className="logout-icon" alt="" src="/logout@2x.png" />
+        <img className="supplier-icon" alt="" src="/supplier@2x.png" />
+        <img className="category-icon" alt="" src="/category@2x.png" />
+        <div className="ordes">
+          <img className="uber-taxi-icon" alt="" src="/uber-taxi@2x.png" />
+          <div className="orders">Orders</div>
+        </div>
+        <div className="expired">
+          <img className="uber-taxi-icon" alt="" src="/expired@2x.png" />
+          <div className="orders">Expired</div>
+        </div>
+        <img className="double-left-icon" alt="" src="/double-left@2x.png" />
+      </div>
+      <div className="frame">
+        <div className="frame1">
+          <div className="header">
+            <div className="bussiness-name">
+              <div className="orders">TUTUMO LIQUOR STORE ONE</div>
+            </div>
+            <div className="login-information">
+              <div className="login-information-child" />
+              <div className="devlesuuda">devlesuuda</div>
+              <div className="admingmailcom">admin@gmail.com</div>
+              <img className="doorbell-icon" alt="" src="/doorbell@2x.png" />
+              <div className="devlesuuda1">devlesuuda</div>
+            </div>
+          </div>
+          <div className="admin-dashboard">
+            <div className="last-six-months">
+              <div className="view">View</div>
+              <div className="div">1, 276, 510.43</div>
+              <div className="last-six-months1">Last six months</div>
+            </div>
+            <div className="daily-sales">
+              <div className="view1">View</div>
+              <div className="ksh-15-32000">Ksh. 15, 320.00</div>
+              <div className="daily-sales1">Daily sales</div>
+              <img className="today-icon" alt="" src="/today@2x.png" />
+            </div>
+            <div className="monthly-sales">
+              <div className="view2">View</div>
+              <div className="div1">380, 486.54</div>
+              <div className="monthly-sales1">Monthly sales</div>
+            </div>
+            <div className="users1">
+              <div className="orders">Users</div>
+              <div className="div2">2</div>
+            </div>
+            <div className="invoices">
+              <div className="orders">Invoices</div>
+              <div className="div2">50</div>
+            </div>
+            <div className="suppliers1">
+              <div className="orders">Suppliers</div>
+              <div className="div2">5</div>
+            </div>
+            <div className="weekly-sales">
+              <div className="ksh-100-20500">Ksh. 100, 205.00</div>
+              <div className="weekly-sales1">Weekly sales</div>
+              <div className="view3">View</div>
+              <img className="week-view-icon" alt="" src="/week-view@2x.png" />
+            </div>
+            <div className="available-products">
+              <div className="div5">10</div>
+              <div className="available-products1">Available Products</div>
+              <img
+                className="liquor-shelf-icon1"
+                alt=""
+                src="/liquor-shelf@2x.png"
+              />
+            </div>
+            <div className="stores1">
+              <div className="div6">2</div>
+              <div className="stores2">Stores</div>
+              <img
+                className="grocery-store-icon1"
+                alt=""
+                src="/grocery-store@2x.png"
+              />
+            </div>
+            <div className="expired-products">
+              <div className="expired-products1">Expired Products</div>
+              <div className="div7">0</div>
+              <img className="expired-icon1" alt="" src="/expired@2x.png" />
+            </div>
+          </div>
+        </div>
+        <img className="supplier-icon1" alt="" src="/supplier@2x.png" />
+        <img className="receipt-icon" alt="" src="/receipt@2x.png" />
+        <img className="month-view-icon" alt="" src="/month-view@2x.png" />
+        <img className="total-sales-icon" alt="" src="/total-sales@2x.png" />
+        <img className="users-icon1" alt="" src="/users@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
